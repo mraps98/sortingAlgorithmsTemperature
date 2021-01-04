25da9804c46b8a5080f20ba1a7519cf079454821
Amritpreet Singh <amritpreet98@gmail.com>
2021-01-04 12:56:36 -0600

    added memory allocatioin for output array in count sort instead of just doing long output[n]

--- algorithms_c/sort/radix.c
+++ algorithms_c/sort/radix.c
@@ -11,7 +11,7 @@ static long getMax(long arr[], int n) {
 }
  
 static void count_sort(long arr[], int n, int exp) {
-    long output[n]; // output array
+    long* output = calloc(n, sizeof(long)); // output array
     int i, count[10] = { 0 };
  
     // Store count of occurrences in count[]
@@ -29,6 +29,8 @@ static void count_sort(long arr[], int n, int exp) {
  
     for (i = 0; i < n; i++)
         arr[i] = output[i];
+
+    free(output);
 }
  
 // The main function to that sorts arr[] of size n using Radix Sort

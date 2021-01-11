e83488589ac3d14642fedaa86c6d3cce47ae73aa
root <root@LAPTOP-KCGN0VJH.localdomain>
2021-01-10 20:39:18 -0600

    initial commit for python

---
+++ algorithms_python/sort/main.py
@@ -0,0 +1,48 @@
+import sys
+
+# Declaring variables
+fileName = ""
+sortType = ""
+numberOfIterations = 1
+dataOriginal = []
+data = []
+
+def readDataFromFile():
+    global fileName, dataOriginal
+    with open(fileName, "r") as f:
+        temp = f.readline()
+        while temp:
+            dataOriginal.append(int(temp))
+            temp = f.readline()
+
+def printAllData():
+    global dataOriginal
+    for n in dataOriginal:
+        print(n)
+
+
+def main():
+    # Global variables
+    global fileName, sortType, numberOfIterations, data, dataOriginal
+
+    #Get Commandline arguments
+    if len(sys.argv) < 3:
+        print("Usage {}, <sortType> <fileName> <numberOfIterations>".format(sys.argv[0]))
+    else:
+        fileName = sys.argv[2] 
+        sortType = sys.argv[1]
+        if len(sys.argv) == 4:
+            numberOfIterations = int(argv[3])
+    
+    # Load data form file
+    readDataFromFile()
+
+    #print data
+    printAllData()
+
+
+    
+
+
+if __name__ == "__main__":
+    main()

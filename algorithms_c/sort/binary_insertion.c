6b3b6d98740cbb84be7fe32159f670cda5450acf
Amritpreet Singh <amritpreet98@gmail.com>
2021-01-12 14:27:14 -0600

    fixed bianry_insertion in c

--- algorithms_c/sort/binary_insertion.c
+++ algorithms_c/sort/binary_insertion.c
@@ -1,38 +1,39 @@
 /* Source https://github.com/TheAlgorithms/C/blob/master/sorting/binary_insertion_sort.c */
 #include <stdlib.h>
 #include "binary_insertion.h"
-static long binary_search(long *arr, long key, long low, long high)
-{
-	if (low >= high)
-		return (key > arr[low]) ? (low + 1) : low;
-	long mid = low + (high - 1) / 2;
-	if (arr[mid] == key)
-		return mid + 1;
-	else if (arr[mid] > key)
-		return binary_search(arr, key, low, mid - 1);
-	else
-		return binary_search(arr, key, mid + 1, high);
+static long binarySearch(long a[], long item, int low, int high) 
+{ 
+    if (high <= low) 
+        return (item > a[low])?  (low + 1): low; 
+  
+    int mid = (low + high)/2; 
+  
+    if(item == a[mid]) 
+        return mid+1; 
+  
+    if(item > a[mid]) 
+        return binarySearch(a, item, mid+1, high); 
+    return binarySearch(a, item, low, mid-1); 
 }
-/*This is where the sorting of the array takes place
- *  arr[] --- Array to be sorted
- *   size --- Array Size
- *    */
-void binsertion_sort(long *arr, long size)
-{
-	long i, j, key, index;
-	for (i = 0; i < size; i++)
-	{
-		j = i - 1;
-		key = arr[i];
-		/* Use binary search to find exact key's index */
-		index = binary_search(arr, key, 0, j);
-		/* Move all elements greater than key from [index...j]  one position */
-		while (j >= index)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		/* Insert key value in right place */
-		arr[j + 1] = key;
-	}
+
+void binsertion_sort(long a[], int n) 
+{ 
+    int i, loc, j, k, selected; 
+  
+    for (i = 1; i < n; ++i) 
+    { 
+        j = i - 1; 
+        selected = a[i]; 
+  
+        // find location where selected sould be inseretd 
+        loc = binarySearch(a, selected, 0, j); 
+  
+        // Move all elements after location to create space 
+        while (j >= loc) 
+        { 
+            a[j+1] = a[j]; 
+            j--; 
+        } 
+        a[j+1] = selected; 
+    } 
 }
--- algorithms_c/sort/binary_insertion.h
+++ algorithms_c/sort/binary_insertion.h
@@ -1,5 +1,5 @@
 /* Source https://github.com/TheAlgorithms/C/blob/master/sorting/binary_insertion_sort.c */
 #ifndef BINARY_INSERTION_H
 #define BINARY_INSERTION_H
-void binsertion_sort(long* arr, long size);
+void binsertion_sort(long a[], int n);
 #endif

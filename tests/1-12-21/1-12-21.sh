0683a76432e939db5dc890dda7287e1d4d412b13
amritpreet98@gmail.com <amritpreet98@gmail.com>
2021-01-15 14:12:58 -0600

    added tests from asus laptop backup

---
+++ tests/1-12-21/1-12-21.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+FILE_NAME="/home/preet/projects/sortingAlgorithmsTemperature/generating_data/rand100k.dat"
+NUMBER_OF_ITERATIONS=10
+PROG_C=/home/preet/projects/sortingAlgorithmsTemperature/algorithms_c/sort/sort_based_on_file.out
+PROG_J="java -cp /home/preet/projects/sortingAlgorithmsTemperature/algorithms_java/sort/ SortBasedOnFile"
+PROG_P="python3 /home/preet/projects/sortingAlgorithmsTemperature/algorithms_python/sort/main.py"
+
+for SORT_TYPE in binsertion bubble bubble_rec cocktail comb cycle gnome heap insertion insertion_rec merge mergebu pancake quick radix selection shell tim
+do
+#	$PROG_C $SORT_TYPE $FILE_NAME $NUMBER_OF_ITERATIONS
+	$PROG_J $SORT_TYPE $FILE_NAME $NUMBER_OF_ITERATIONS
+#	$PROG_P $SORT_TYPE $FILE_NAME $NUMBER_OF_ITERATIONS
+done
